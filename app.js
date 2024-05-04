58c17b17893155519a4ac75a5a333e267a9808d0
Salman Tyagi <salmantyagi14@gmail.com>
2024-05-04 10:41:53 +0530

    added security packages as helmet for secure headers, express-rate-limit for multiple requests from the same IP

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
-import cors from 'cors';
+// import cors from 'cors';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 import userRoutes from './routes/userRoutes.js';
 import cityRoutes from './routes/cityRoutes.js';
@@ -13,17 +15,37 @@ import globalErrorHandler from './controllers/globalError.js';
 
 const app = express();
 
+// Helmet sets headers for security purpose
+app.use(helmet());
+
+// Rate limit for multiple requests from the same IP
+app.use(
+  rateLimit({
+    windowMs: 60 * 60 * 1000,
+    limit: 100,
+    message: 'Too many requests! Try again later after 1 hour',
+  })
+);
+
 // morgan (logger)
 app.use(morgan('dev'));
 
 // Body parser
-app.use(express.json());
+app.use(
+  express.json({
+    limit: '5kb',
+  })
+);
 
 // Static files
 app.use(express.static('public'));
 
 // To allow access resources request from other origin (domain)
-app.use(cors());
+// For simple get, post requests
+// app.use(cors());
+
+// For pre-flight phase options requests, checks is it safe before executive patch, put, delete other requests
+// app.options('/api/v1/', cors());
 
 // Middleware
 app.use((req, res, next) => {
@@ -40,7 +62,7 @@ app.use('/api/v1/bookings', bookingRoutes);
 app.use('/api/v1/ratings', ratingRoutes);
 
 // All unknown requests
-app.use('*', (req, res, next) =>
+app.all('*', (req, res, next) =>
   next(new AppError(`${req.originalUrl} not found`, 404))
 );
 
